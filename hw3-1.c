7225e5b9bafd21eeb2934c14ef739bc99c7b0cbb
112310203 <146064960+112310203@users.noreply.github.com>
2023-10-23 11:45:18 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,21 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main()
+int is_prime(int);
+int main(int argc, char *argv[])
 {
-   int n;
-   scanf("%d",&n);
-   int i;
-   for(i=2;i<n;i++){
-    if(n%i==0){
-printf("no");
-break;
-    }
-    }
- if(n==i) {
-printf("yes");
+	int number;
+	scanf("%d",&number);
+	if(is_prime(number)){
+		printf("此數是質數\n");
+	}else{
+		printf("此數不是質數\n");
+	}
+  
+  system("PAUSE");	 
+  return 0;
 }
-
-
+int is_prime(int num){
+	int i;
+	if(num==1){
+		return 0;
+	}else{
+		for(i=2;i<num;i++){
+			if(num%i==0){
+				return 0;
+			}
+		}
+	}
+	return 1;
 }
-
