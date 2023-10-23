759ff77ccfe2de3f823f34ff68335dbde64d854c
112310203 <146064960+112310203@users.noreply.github.com>
2023-10-24 00:07:56 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -1,20 +1,38 @@
 #include <stdio.h>
-main()
+#include <stdlib.h>
+
+int main()
 {
-int i, j, k,l,m,n,o;
-scanf("%d%d%d%d%d%d", &i, &j, &k,&l,&m,&n);
+    int d, e;
+    scanf("%d %d", &d, &e);
+    int a1, a2, a3, a4, b1, b2, b3, b4;
+    a1 = d / 1000;
+    a2 = (d - a1 * 1000) / 100;
+    a3 = (d - a1 * 1000 - a2 * 100) / 10;
+    a4 = (d - a1 * 1000 - a2 * 100 - a3 * 10);
 
-if (k<j){
-    o++;
-}
-if(m<l){
-    o++;
-}
-if(m<j){
-    o++;
-}
+    b1 = e/ 1000;
+    b2 = (e - b1 * 1000) / 100;
+    b3 = (e - b1 * 1000 - b2 * 100) / 10;
+    b4 = (e - b1 * 1000 - b2 * 100 - b3 * 10);
 
+    int f,g ;
+    f= a1 + a2 + a3 + a4;
+    g = b1 + b2 + b3 + b4;
 
-printf("%d\n", o);
-return 0;
+    if(f>g){
+        printf("%d\n",e);
+    }
+    else if(f<g){
+        printf("%d\n",d);
+    }
+    else{
+        if(d>e){
+            printf("%d\n",e);
+        }
+        else if(d<e){
+            printf("%d\n",d);
+        }
+
+    }
 }
