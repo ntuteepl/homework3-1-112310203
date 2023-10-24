fbb498f6e25ea182815bba1703fa4efc229097e9
112310203 <146064960+112310203@users.noreply.github.com>
2023-10-24 09:16:50 +0800

    Update hw3-2.c

--- hw3-2.c
+++ hw3-2.c
@@ -7,20 +7,10 @@ scanf("%d%d%d%d%d%d",&s1,&d1,&s2,&d2,&s3,&d3);
 if(d1==s2&&d2==s3||d2==s3&&d3==s1||d3==s1&&d1==s2||d1==s3&&d3==s2||d2==s1&&d1==s3||d3==s2&&d2==s1){
      printf("1");
     }
-else if(s1==d2==d3||s2==d3==d1||s3==d1==d2){
+else if(s1!=d2!=d3||s2!=d3!=d1||s3!=d1!=d2){
      printf("3");
 }
 else{
      printf("2");
 }
-}
-
-        if(d>e){
-            printf("%d\n",e);
-        }
-        else if(d<e){
-            printf("%d\n",d);
-        }
-
-    }
 }
